1b49475af63cdf069a7b1635f26fabda62e6e04b
Hack Reactor Student <student@hackreactor.com>
2017-04-07 15:46:36 -0700

    Passed most of the specs - 2 remaining

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,59 @@
-// YOUR CODE HERE:
+var app = {
+  server: "http://parse.sfm8.hackreactor.com/chatterbox/classes/messages",
+
+	init: function() {
+
+	},
+
+	send: function(message) {
+		$.ajax({
+		// This is the url you should use to communicate with the parse API server.
+		url: this.server,
+		type: 'POST',
+		data: JSON.stringify(message),
+		contentType: 'application/json',
+		success: function (data) {
+		console.log('chatterbox: Message sent');
+		},
+		error: function (data) {
+		// See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+		console.error('chatterbox: Failed to send message', data);
+		}
+		});
+	},
+
+  fetch: function() {
+    $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+    console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message', data);
+    }
+    });
+  },
+
+  clearMessages: function() {
+    $('#chats').empty();
+  },
+
+  renderMessage: function(message) {
+    // Get the #chats element
+    // Create a new element and prepend to #chats
+    let $paragraph = ('<p>' + message.text + '</p>');
+    $('#chats').prepend($paragraph);
+
+
+  },
+
+  renderRoom: function(room) {
+    let $room = ('<option value=' + room + '>' + room + '</option>');
+    $('#roomSelect').append($room);
+  }
+}
+
